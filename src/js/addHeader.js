ee3a20c2152690a873216dfe943833c11a3c82d4
Braingenius2 <braingenius2@gmail.com>
2023-05-31 01:20:01 +0300

    Implement addHeader

--- src/js/addHeader.js
+++ src/js/addHeader.js
@@ -1,6 +1,6 @@
 const addHeader = () => {
   const pageBody = document.querySelector('body');
-  const footerElement = document.createElement('header');
+  const headerElement = document.createElement('header');
   headerElement.classList.add('header');
   headerElement.innerhtml = `<img src="./images/top-movies-logo.png" alt="top movies logo">`;
 
--- src/js/pagelayout.js
+++ src/js/pagelayout.js
@@ -1,10 +1,12 @@
 import showCards from './showcards.js';
+import addHeader from './addheader.js';
 import addFooter from './addFooter.js';
 
 
 const pageLayout = () => {
   const mainContainer = document.querySelector('.main-container');
 
+  addHeader();
   showCards(mainContainer);
   addFooter();
 };
