bc42b1809595fca047e809248d86991786646655
Braingenius2 <braingenius2@gmail.com>
2023-05-31 06:33:47 +0300

    Edit pagelayout.js

--- src/js/pagelayout.js
+++ src/js/pagelayout.js
@@ -1,6 +1,6 @@
 import showCards from './showcards.js';
-import addHeader from './addHeader.js';
 import addFooter from './addFooter.js';
+import addHeader from './addHeader.js';
 
 const pageLayout = () => {
   const mainContainer = document.querySelector('.main-container');
