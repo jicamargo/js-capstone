c37289c459fab990888e74b6aff5c85be5c31edb
Braingenius2 <braingenius2@gmail.com>
2023-05-31 04:54:50 +0300

    Fix bug

--- src/js/addHeader.js
+++ src/js/addHeader.js
@@ -3,7 +3,7 @@ import topMoviesLogo from '../images/top-movies-logo.png';
 const addHeader = () => {
   const headerElement = document.querySelector('header');
   headerElement.classList.add('header');
-  headerElement.innerHTML = `<img src=${topMoviesLogo} alt="top movies logo>`;
+  headerElement.innerHTML = `<img src='${topMoviesLogo}' alt="top movies logo">`;
 };
 
 export default addHeader;
