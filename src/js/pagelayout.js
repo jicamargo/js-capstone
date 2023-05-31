8502cb5782a16ef7116de233055cf05df4a7b0ff
Braingenius2 <braingenius2@gmail.com>
2023-05-31 05:59:09 +0300

    Fix eslint errors

--- src/js/pagelayout.js
+++ src/js/pagelayout.js
@@ -2,7 +2,6 @@ import showCards from './showcards.js';
 import addHeader from './addHeader.js';
 import addFooter from './addFooter.js';
 
-
 const pageLayout = () => {
   const mainContainer = document.querySelector('.main-container');
 
