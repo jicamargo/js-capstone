20ffe8b23ffa69cd99a7f0d8621b5d0988d0a940
Braingenius2 <braingenius2@gmail.com>
2023-05-31 19:54:28 +0300

    Fix eslint error. Change module name to lower case

--- src/js/pagelayout.js
+++ src/js/pagelayout.js
@@ -1,6 +1,6 @@
 import showCards from './showcards.js';
-import addFooter from './addFooter.js';
-import addHeader from './addHeader.js';
+import addFooter from './addfooter.js';
+import addHeader from './addheader.js';
 
 const pageLayout = () => {
   const mainContainer = document.querySelector('.main-container');
