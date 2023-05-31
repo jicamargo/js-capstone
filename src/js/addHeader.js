ba399c1832567014c4068f7b3e80e1cf3ae48004
Braingenius2 <braingenius2@gmail.com>
2023-05-31 19:52:25 +0300

    Add navlinks

--- src/js/addHeader.js
+++ src/js/addHeader.js
@@ -3,7 +3,12 @@ import topMoviesLogo from '../images/top-movies-logo.png';
 const addHeader = () => {
   const headerElement = document.querySelector('header');
   headerElement.classList.add('header');
-  headerElement.innerHTML = `<img src='${topMoviesLogo}' alt="top movies logo">`;
+  headerElement.innerHTML = `<img src='${topMoviesLogo}' alt="top movies logo">
+                              <nav class="navlinks">
+                              <li>Home</li>
+                              <li>Shows</li>
+                              <li>About</li>
+                              </nav>`;
 };
 
 export default addHeader;
