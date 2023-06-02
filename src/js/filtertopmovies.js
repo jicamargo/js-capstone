26c05c82da76671175393e498fa44b199c5dd1e4
jic <ji_camargo@yahoo.com>
2023-06-02 10:36:07 -0500

    Fix ES linter error

--- src/js/filtertopmovies.js
+++ src/js/filtertopmovies.js
@@ -1,16 +1,16 @@
 import readAPI from './readapi.js';
 
-// Create an array of movies from the movies object
-// Sort the movies in descending order based on their ratings
-// Get the top 10 movies from the sorted array
+const filterTopMovies = () => readAPI()
+  .then((movies) => {
+    // Create an array of movies from the movies object
+    const movieArray = Object.values(movies);
+    // Sort the movies in descending order based on their ratings
+    const sortedMovies = movieArray.sort((a, b) => b.rating - a.rating);
+    // Get the top 10 movies from the sorted array
+    const top10Movies = sortedMovies.slice(0, 10);
 
-const filterTopMovies = () => readAPI().then((movies) => {
-  const movieArray = Object.values(movies);
-  const sortedMovies = movieArray.sort((a, b) => b.rating - a.rating);
-  const top10Movies = sortedMovies.slice(0, 10);
-
-  return top10Movies;
-})
+    return top10Movies;
+  })
   .catch((error) => {
     throw new Error(`An error occurred while filtering top movies: ${error}`);
   });
